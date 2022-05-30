07fea944d9c64d9b2d42d34213afdb3f6e226ca2
shramitgupta <76399598+shramitgupta@users.noreply.github.com>
2022-05-30 11:58:13 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/codewithcal/au/calculatorappexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/codewithcal/au/calculatorappexample/ExampleInstrumentedTest.java
@@ -19,8 +19,8 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
+        // Context of the app under test .
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("codewithcal.au.calculatorappexample", appContext.getPackageName());
+        assertEquals("codewithcal.au.calculatorappexample", appContext.getPackageName());//context
     }
 }
